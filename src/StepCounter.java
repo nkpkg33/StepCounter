d3be00a977da360bc07bdfe03b411945bc0696e0
Neha Konakalla <nehakonakalla@Nehas-MacBook.local>
2016-11-04 11:05:22 -0700

    added second improvement

--- src/StepCounter.java
+++ src/StepCounter.java
@@ -1,13 +1,14 @@
 
 public class StepCounter {
-	
-	public static void main (String[] args){
-		
-		
+
+	public static void main(String[] args) {
+
 	}
 
 	/***
 	 * Counts the number of steps based on sensor data
+	 * 
+	 * Our first idea which counts a step only if a peak crosses a peak threshold followed by a trough which crosses a trough threshold
 	 * 
 	 * @param times
 	 *            a 1d array with the elapsed times in milliseconds for each row
@@ -38,27 +39,96 @@ public class StepCounter {
 		double[] magnitudes = calculateMagnitudesFor(smallerData);
 
 		int peakCounter = 0;
-		
+
 		boolean crossedPeakThreshold = false;
+
+		double peakThreshold = .75 * (calculateStandardDeviation(magnitudes, calculateMean(magnitudes)));
+
+		double troughThreshold = .25 * (calculateStandardDeviation(magnitudes, calculateMean(magnitudes)));
 
 		for (int i = 1; i < magnitudes.length - 1; i++) {
 
 			if (magnitudes[i] > magnitudes[i - 1] && magnitudes[i] > magnitudes[i + 1]
-					&& magnitudes[i] > calculateMean(magnitudes) + .7 * (calculateStandardDeviation(magnitudes, calculateMean(magnitudes))) ){
-			
-			crossedPeakThreshold = true;
+					&& magnitudes[i] > calculateMean(magnitudes) + peakThreshold) {
+
+				crossedPeakThreshold = true;
+
+			}
+
+			else if (magnitudes[i] < magnitudes[i - 1] && magnitudes[i] < magnitudes[i + 1]
+					&& magnitudes[i] < calculateMean(magnitudes) - troughThreshold && crossedPeakThreshold) {
+
+				peakCounter++;
+
+				crossedPeakThreshold = false;
+
+			}
+		}
+
+		return peakCounter;
+
+	}
+	
+	/***
+	 * Our second idea which takes the average of the peaks and troughs
+	 * @param times  a 1d array with the elapsed times in milliseconds for each row
+	 *            in the sensorData array
+	 * @param sensorData  a 2D-array where rows represent successive sensor data
+	 *            samples, and the columns represent different sensors. We
+	 *            assume there are 6 columns. Columns 0-2 are data from the x,
+	 *            y, and z axes of an accelerometer, and 3-5 are data from the
+	 *            x,y, and z axes of a gyro
+	 * @return an double representing the number of steps
+	 */
+
+	public static double countStepsWithAverage(double[] times, double[][] sensorData) {
+
+		double[][] smallerData = new double[sensorData.length][3];
+
+		for (int row = 0; row < sensorData.length; row++) {
+
+			for (int col = 0; col < 2; col++) {
+
+				smallerData[row][col] = sensorData[row][col];
+
+			}
 
 		}
-			
-			else if (magnitudes[i] < magnitudes[i - 1] && magnitudes[i] < magnitudes[i + 1] && magnitudes[i] < calculateMean(magnitudes) - .25 * (calculateStandardDeviation(magnitudes, calculateMean(magnitudes))) && crossedPeakThreshold){
-			
+
+		double[] magnitudes = calculateMagnitudesFor(smallerData);
+
+		int peakCounter = 0;
+
+		int troughCounter = 0;
+
+		boolean crossedPeakThreshold = false;
+
+		double peakThreshold = .75 * (calculateStandardDeviation(magnitudes, calculateMean(magnitudes)));
+
+		double troughThreshold = .25 * (calculateStandardDeviation(magnitudes, calculateMean(magnitudes)));
+
+		for (int i = 1; i < magnitudes.length - 1; i++) {
+
+			if (magnitudes[i] > magnitudes[i - 1] && magnitudes[i] > magnitudes[i + 1]
+					&& magnitudes[i] > calculateMean(magnitudes) + peakThreshold) {
+
 				peakCounter++;
-				
+
+				crossedPeakThreshold = true;
+
+			}
+
+			else if (magnitudes[i] < magnitudes[i - 1] && magnitudes[i] < magnitudes[i + 1]
+					&& magnitudes[i] < calculateMean(magnitudes) - troughThreshold && crossedPeakThreshold) {
+
+				troughCounter++;
+
 				crossedPeakThreshold = false;
-			
-		}}
-		
-		return peakCounter;
+
+			}
+		}
+
+		return Math.round((peakCounter + troughCounter) / (double) 2);
 
 	}
 
@@ -111,21 +181,21 @@ public class StepCounter {
 		return sum / arr.length;
 
 	}
-	
-	public static void replaceAbsoluteTimeWithElapsedTime(double[][] sensorData){
-		
+
+	public static void replaceAbsoluteTimeWithElapsedTime(double[][] sensorData) {
+
 		double previous = sensorData[0][0];
-		
+
 		sensorData[0][0] = 0;
-		
-		for (int i = 1; i < sensorData.length; i++){
-			
+
+		for (int i = 1; i < sensorData.length; i++) {
+
 			sensorData[i][0] = sensorData[i][0] - previous;
-			
+
 			previous = sensorData[i][0];
-			
+
 		}
-		
+
 	}
 
 }
