6b1cc5b83c2aaac17a5b12b9c87d9073e03ad744
Neha Konakalla <nehakonakalla@nehas-macbook.attlocal.net>
2016-11-11 18:46:05 -0800

    updated JavaDoc Comments

--- src/StepCounter.java
+++ src/StepCounter.java
@@ -1,3 +1,6 @@
+import javax.swing.JFrame;
+
+import org.math.plot.Plot2DPanel;
 
 public class StepCounter {
 
@@ -8,7 +11,8 @@ public class StepCounter {
 	/***
 	 * Counts the number of steps based on sensor data
 	 * 
-	 * Our first idea which counts a step only if a peak crosses a peak threshold followed by a trough which crosses a trough threshold
+	 * Our first idea which counts a step only if a peak crosses a peak
+	 * threshold followed by a trough which crosses a trough threshold
 	 * 
 	 * @param times
 	 *            a 1d array with the elapsed times in milliseconds for each row
@@ -24,19 +28,7 @@ public class StepCounter {
 
 	public static int countSteps(double[] times, double[][] sensorData) {
 
-		double[][] smallerData = new double[sensorData.length][3];
-
-		for (int row = 0; row < sensorData.length; row++) {
-
-			for (int col = 0; col < 2; col++) {
-
-				smallerData[row][col] = sensorData[row][col];
-
-			}
-
-		}
-
-		double[] magnitudes = calculateMagnitudesFor(smallerData);
+		double magnitudes[] = extractData(sensorData);
 
 		int peakCounter = 0;
 
@@ -68,12 +60,15 @@ public class StepCounter {
 		return peakCounter;
 
 	}
-	
+
 	/***
 	 * Our second idea which takes the average of the peaks and troughs
-	 * @param times  a 1d array with the elapsed times in milliseconds for each row
+	 * 
+	 * @param times
+	 *            a 1d array with the elapsed times in milliseconds for each row
 	 *            in the sensorData array
-	 * @param sensorData  a 2D-array where rows represent successive sensor data
+	 * @param sensorData
+	 *            a 2D-array where rows represent successive sensor data
 	 *            samples, and the columns represent different sensors. We
 	 *            assume there are 6 columns. Columns 0-2 are data from the x,
 	 *            y, and z axes of an accelerometer, and 3-5 are data from the
@@ -83,19 +78,7 @@ public class StepCounter {
 
 	public static double countStepsWithAverage(double[] times, double[][] sensorData) {
 
-		double[][] smallerData = new double[sensorData.length][3];
-
-		for (int row = 0; row < sensorData.length; row++) {
-
-			for (int col = 0; col < 2; col++) {
-
-				smallerData[row][col] = sensorData[row][col];
-
-			}
-
-		}
-
-		double[] magnitudes = calculateMagnitudesFor(smallerData);
+		double magnitudes[] = extractData(sensorData);
 
 		int peakCounter = 0;
 
@@ -132,6 +115,63 @@ public class StepCounter {
 
 	}
 
+	/***
+	 * Counts the number of steps based on sensor data.
+	 * 
+	 * @param times
+	 *            a 1d-array with the elapsed times in miliseconds for each row
+	 *            in the sensorData array.
+	 * 
+	 * @param sensorData
+	 *            a 2d-array where rows represent successive sensor data
+	 *            samples, and the columns represent different sensors. We
+	 *            assume there are 6 columns. Columns 0 - 2 are data from the x,
+	 *            y, and z axes of an accelerometer, and 3-5 are data from the
+	 *            x, y, and z axes of a gyro.
+	 * 
+	 * @return an int representing the number of steps
+	 */
+	public static int countStepsOriginal(double[] times, double[][] sensorData) {
+
+		double magnitudes[] = extractData(sensorData);
+
+		int peakCounter = 0;
+
+		for (int i = 1; i < magnitudes.length - 1; i++) {
+
+			if (magnitudes[i] > magnitudes[i - 1] && magnitudes[i] > magnitudes[i + 1]
+					&& magnitudes[i] > calculateMean(magnitudes)
+							+ .7 * (calculateStandardDeviation(magnitudes, calculateMean(magnitudes)))) {
+
+				peakCounter++;
+
+			}
+		}
+
+		return peakCounter;
+
+	}
+
+	public static double[] extractData(double[][] sensorData) {
+
+		double[][] smallerData = new double[sensorData.length][3];
+
+		for (int row = 0; row < sensorData.length; row++) {
+
+			for (int col = 0; col < 2; col++) {
+
+				smallerData[row][col] = sensorData[row][col];
+
+			}
+
+		}
+
+		double[] magnitudes = calculateMagnitudesFor(smallerData);
+
+		return magnitudes;
+
+	}
+
 	public static double calculateMagnitude(double x, double y, double z) {
 
 		return Math.sqrt((x * x) + (y * y) + (z * z));
@@ -152,7 +192,7 @@ public class StepCounter {
 
 	}
 
-	private static double calculateStandardDeviation(double[] arr, double mean) {
+	public static double calculateStandardDeviation(double[] arr, double mean) {
 
 		double sum = 0;
 
@@ -168,7 +208,7 @@ public class StepCounter {
 
 	}
 
-	private static double calculateMean(double[] arr) {
+	public static double calculateMean(double[] arr) {
 
 		double sum = 0;
 
@@ -182,7 +222,7 @@ public class StepCounter {
 
 	}
 
-	public static void replaceAbsoluteTimeWithElapsedTime(double[][] sensorData) {
+	public static double[][] replaceAbsoluteTimeWithElapsedTime(double[][] sensorData) {
 
 		double previous = sensorData[0][0];
 
@@ -190,11 +230,17 @@ public class StepCounter {
 
 		for (int i = 1; i < sensorData.length; i++) {
 
+			double temp = sensorData[i][0];
+
 			sensorData[i][0] = sensorData[i][0] - previous;
 
-			previous = sensorData[i][0];
+			previous = temp;
+
+			System.out.println(sensorData[i][0]);
 
 		}
+
+		return sensorData;
 
 	}
 
